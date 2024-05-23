7207ecb507a7439b9a43067eddb4bd8e0d52d81a
VJean <jean@vintache.eu>
2024-05-23 22:04:29 +0200

    add Telescope key bindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,3 +27,10 @@ require("lazy").setup({
     },
 })
 
+-- Telescope
+local tsbuiltin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', tsbuiltin.find_files, {})
+vim.keymap.set('n', '<leader>fg', tsbuiltin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', tsbuiltin.buffers, {})
+vim.keymap.set('n', '<leader>fh', tsbuiltin.help_tags, {})
+
