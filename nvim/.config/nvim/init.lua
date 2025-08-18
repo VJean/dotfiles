9415d8c0b76f735b2c84e7546d517600b0c89025
Jean Vintache <jean.vintache@datadoghq.com>
2025-08-18 14:14:17 +0200

    add nvim-tree and update plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,6 +30,17 @@ require("lazy").setup({
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    {
         "catppuccin/nvim", name = "catppuccin", priority = 1000
     },
     {
